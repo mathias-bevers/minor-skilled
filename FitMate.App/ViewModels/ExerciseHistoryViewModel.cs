3f961eeb75140bfb1eaaec618a47e56584edd8f9
Mathias Bevers <mathiasbevers@outlook.com>
2024-09-26 10:25:14 +0200

    created history page
    - designed history page.
    - created and added fake data to history page.
    - history page layout is not a desired (yet).

---
+++ FitMate.App/ViewModels/ExerciseHistoryViewModel.cs
@@ -0,0 +1,50 @@
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using FitMate.Models;
+using FitMate.ViewModels.Mockups;
+
+namespace FitMate.ViewModels;
+
+public class ExerciseHistoryViewModel : ObservableObject
+{
+    public ObservableCollection<ExerciseGroupMockup> Exercises { get; set; }
+    public PersonalRecordMockup PR { get; set; }
+
+    public ExerciseHistoryViewModel()
+    {
+        PR = new PersonalRecordMockup
+        {
+            ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 12, 10)
+        };
+
+        Exercises = new ObservableCollection<ExerciseGroupMockup>([
+            new ExerciseGroupMockup(DateTime.Today.ToShortDateString(), [
+                new ExerciseMockup { ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 10, 10) },
+                new ExerciseMockup { ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 10, 10) },
+                new ExerciseMockup
+                {
+                    ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 12, 10),
+                    IsPersonalRecord = true
+                }
+            ]),
+            
+            new ExerciseGroupMockup(DateTime.Today.ToShortDateString(), [
+                new ExerciseMockup { ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 10, 10) },
+                new ExerciseMockup { ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 10, 10) },
+                new ExerciseMockup { ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 10, 10) },
+            ]),
+            
+            new ExerciseGroupMockup(DateTime.Today.ToShortDateString(), [
+                new ExerciseMockup { ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 10, 10) },
+                new ExerciseMockup { ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 10, 10) },
+                new ExerciseMockup { ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 10, 10) },
+            ]),
+            
+            new ExerciseGroupMockup(DateTime.Today.ToShortDateString(), [
+                new ExerciseMockup { ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 8, 10) },
+                new ExerciseMockup { ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 8, 10) },
+                new ExerciseMockup { ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 8, 10) },
+            ])
+        ]);
+    }
+}
--- FitMate.App/Views/ExerciseHistoryPage.xaml.cs
+++ FitMate.App/Views/ExerciseHistoryPage.xaml.cs
@@ -8,9 +8,14 @@ namespace FitMate.Views;
 
 public partial class ExerciseHistoryPage : ContentPage
 {
+    ViewModels.ExerciseHistoryViewModel viewModel;
+    
     public ExerciseHistoryPage()
     {
         InitializeComponent();
         Title = "History";
+        
+        viewModel = new ViewModels.ExerciseHistoryViewModel();
+        BindingContext = viewModel;
     }
 }
