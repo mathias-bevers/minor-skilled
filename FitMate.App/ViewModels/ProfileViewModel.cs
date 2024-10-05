a7b37d85fa79a3f6b80a8760914324e38854dd0d
Mathias Bevers <mathiasbevers@outlook.com>
2024-10-05 19:41:21 +0200

    loaded prs in the profile page
    - when on appearing the prs are loaded.
    - by down grading rider - due to emulator issue - renamed vm:Mockups.<T>
    -> mockups:<T> in a lot of xaml pages.

--- FitMate.App/ViewModels/ProfileViewModel.cs
+++ FitMate.App/ViewModels/ProfileViewModel.cs
@@ -1,47 +1,28 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using FitMate.Models;
-using FitMate.ViewModels.Mockups;
 using Microsoft.Data.SqlClient;
 
 namespace FitMate.ViewModels;
 
 public partial class ProfileViewModel : ObservableObject
 {
-    public ObservableCollection<PersonalRecordMockup> PlaceholderPRs { get; set; }
+    public ObservableCollection<Exercise> PersonalRecords { get; set; } = [];
 
     [ObservableProperty]
     private User user = new();
-    
-    public ProfileViewModel()
-    {
-        PlaceholderPRs = new ObservableCollection<PersonalRecordMockup>([
-            new PersonalRecordMockup
-            {
-                Name = "Bicep Curl",
-                ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 14, 12)
-            },
-            new PersonalRecordMockup
-            {
-                Name = "Treadmill",
-                ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.MeterMinutes, 1320, 422)
-            },
-            new PersonalRecordMockup
-            {
-                Name = "Leg Press",
-                ExerciseSet = new ExerciseSet((int)ExerciseSet.SetType.KiloReps, 160, 10)
-            }
-        ]);
-    }
 
     public void LoadUserFromDB()
     {
         User tmp = new();
+        List<Exercise> tmpList = [];
+        PersonalRecords.Clear();
+
         using (SqlConnection connection = new(App.SERVER_SETTINGS.ConnectionString))
         {
             connection.Open();
 
-            
+
             using (SqlCommand command = new(GetUserQuery(), connection))
             {
                 SqlDataReader reader = command.ExecuteReader();
@@ -57,17 +38,51 @@ public partial class ProfileViewModel : ObservableObject
                 }
             }
 
+            using (SqlCommand command = new(GetPersonalRecordsQuery(), connection))
+            {
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int kgsOrMtr = (int)reader["KgsOrMtr"];
+                        int repsOrSecs = (int)reader["RepsOrSecs"];
+                        string name = reader["Name"].ToString() ?? "ERROR";
+                        int measurement = (int)reader["MeasurementTypeID"];
+                        
+                        Exercise exercise = new()
+                        {
+                            KgsOrMtr = kgsOrMtr,
+                            RepsOrSecs = repsOrSecs,
+                            ExerciseType = new ExerciseType
+                            {
+                                Name = name,
+                                MeasurementTypeID = measurement
+                            }
+                        };
+                        tmpList.Add(exercise);
+                    }
+                }
+            }
+
             connection.Close();
         }
         
         User = tmp;
+
+        for (int i = 0; i < tmpList.Count; ++i)
+        {
+            PersonalRecords.Add(tmpList[i]);
+        }
     }
 
-    private string GetUserQuery() =>
+    private static string GetUserQuery() =>
         $"SELECT u.UserName, u.GenderID, u.DateOfBirth FROM Users u WHERE ID = {App.USER_ID}";
 
-    private string GetPersonalRecordsQuery()
-    {
-        
-    }
+    private static string GetPersonalRecordsQuery() =>
+        "SELECT e.KgsOrMtr, e.RepsOrSecs, et.Name, et.MeasurementTypeID " +
+        "FROM Exercise e JOIN Workouts w ON e.WorkoutID  = w.ID " +
+        "JOIN Users u ON u.ID = w.UserID JOIN ExercisesTypes et ON " +
+        $"e.ExerciseTypeID = et.ID WHERE u.ID = {App.USER_ID} AND e.IsPR = 1;";
 }
