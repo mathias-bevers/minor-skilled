b17b77853949bc7fee6014d5cdb49c9cb124a6fe
Mathias Bevers <mathiasbevers@protonmail.com>
2026-01-08 12:49:59 +0100

    exercises are correctly loaded int the workout view
    - exercises are now loaded correcly while using the sql-communicator.
    - removed the 'e.IsPR' column from the query
    - updated allworkoutsview since the aggregate function was not correct.

--- FitMate.App/ViewModels/WorkoutModelView.cs
+++ FitMate.App/ViewModels/WorkoutModelView.cs
@@ -1,75 +1,75 @@
 using System.Collections.ObjectModel;
 using System.Data.SqlTypes;
 using CommunityToolkit.Mvvm.ComponentModel;
+using FitMate.Utils;
 using Microsoft.Data.SqlClient;
 
 namespace FitMate.ViewModels;
 
 public partial class WorkoutModelView : ObservableObject, IQueryAttributable
 {
-    public ObservableCollection<ExerciseGroup> Exercises { get; set; } = [];
     public int WorkoutID { get; private set; } = -1;
+    public ObservableCollection<ExerciseGroup> Exercises { get; set; } = [];
 
     [ObservableProperty]
     public string emptyWorkoutMessage = "Loading ...";
 
     public void ApplyQueryAttributes(IDictionary<string, object> query)
     {
-        if (query.TryGetValue("id", out object? value)) { WorkoutID = (int)value; }
-
-        //TODO: add create workout tab.
-
-        if (WorkoutID < 0) { throw new InvalidOperationException("Workout ID needs to be set at least once!"); }
-
-        Exercises.Clear();
-        Task.Run(LoadWorkoutAsync);
-    }
-
-    public async Task LoadWorkoutAsync()
-    {
-        List<Models.Exercise> exercises = [];
-        EmptyWorkoutMessage = "Loading ...";
-
-        await using SqlConnection connection = new(App.SETTINGS.Server.ConnectionString);
-
-        connection.Open();
-
-        await using (SqlCommand command = new(GenerateLoadWorkoutQuery(), connection))
+        if (query.TryGetValue("id", out object? value))
         {
-            SqlDataReader reader = await command.ExecuteReaderAsync();
-
-            if (!reader.HasRows)
-            {
-                EmptyWorkoutMessage = "No exercises logged!";
-                connection.Close();
-                return;
-            }
-
-            while (reader.Read())
-            {
-                exercises.Add(new Models.Exercise
-                {
-                    KgsOrMtr = Convert.ToInt32(reader["KgsOrMtr"]),
-                    RepsOrSecs = Convert.ToInt32(reader["RepsOrSecs"]),
-                    IsPR = Convert.ToBoolean(reader["IsPR"]),
-                    ExerciseType = new Models.ExerciseType
-                    {
-                        ID = Convert.ToInt32(reader["ID"]),
-                        Name = Convert.ToString(reader["Name"]) ??
-                               throw new SqlNullValueException("The ExerciseType.Name is null!"),
-                        MeasurementTypeID = Convert.ToInt32(reader["MeasurementTypeID"])
-                    }
-                });
-            }
+            WorkoutID = (int)value;
         }
 
-        connection.Close();
+        if (WorkoutID < 0)
+        {
+            throw new InvalidOperationException("Workout ID needs to be set at least once!");
+        }
 
-        foreach (ExerciseGroup group in exercises.GroupBy(e => e.ExerciseType.Name)
-                     .Select(g => new ExerciseGroup(g.Key.ToString(), g.ToList()))) { Exercises.Add(group); }
+        LoadWorkout();
     }
 
-    private string GenerateLoadWorkoutQuery() =>
-        "SELECT e.KgsOrMtr, e.RepsOrSecs, e.IsPR, et.ID, et.Name, et.MeasurementTypeID " +
-        "FROM Exercises e JOIN ExerciseTypes et ON e.ExerciseTypeID = et.ID WHERE WorkoutID = " + WorkoutID;
+    private void LoadWorkout()
+    {
+        Exercises.Clear();
+
+        List<Models.Exercise> exercises = [];
+
+        EmptyWorkoutMessage = "Loading ...";
+
+        SqlCommand command = new("SELECT e.KgsOrMtr, e.RepsOrSecs, et.ID, et.Name, et.MeasurementTypeID " +
+                                 "FROM Exercises e " +
+                                 "JOIN ExerciseTypes et ON e.ExerciseTypeID = et.ID " +
+                                 "WHERE WorkoutID = @wID");
+        command.Parameters.AddWithValue("@wID", WorkoutID);
+
+        Task<bool> task = Task.Run(() => SqlCommunicator.Select(command, reader =>
+        {
+            exercises.Add(new Models.Exercise
+            {
+                KgsOrMtr = Convert.ToInt32(reader["KgsOrMtr"]),
+                RepsOrSecs = Convert.ToInt32(reader["RepsOrSecs"]),
+                ExerciseType = new Models.ExerciseType
+                {
+                    ID = Convert.ToInt32(reader["ID"]),
+                    Name = Convert.ToString(reader["Name"]) ??
+                           throw new SqlNullValueException("The ExerciseType.Name is null!"),
+                    MeasurementTypeID = Convert.ToInt32(reader["MeasurementTypeID"])
+                }
+            });
+        }));
+
+        if (task.Result)
+        {
+            foreach (ExerciseGroup group in exercises.GroupBy(e => e.ExerciseType.Name)
+                         .Select(g => new ExerciseGroup(g.Key.ToString(), g.ToList())))
+            {
+                Exercises.Add(group);
+            }
+        }
+        else
+        {
+            EmptyWorkoutMessage = "no exercises logged...";
+        }
+    }
 }
