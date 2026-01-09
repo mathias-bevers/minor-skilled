8a6bab896f7a8c9ebfd992095d906ba3f2c22eeb
Mathias Bevers <mathiasbevers@protonmail.com>
2026-01-09 18:19:59 +0100

    the prs are shown on the workout page
    - the pr trophy icon is shown on the workout page
    - loaded the pr by id after all of the normal exercises are loaded and
    checked if the id was a pr id.

--- FitMate.App/ViewModels/WorkoutModelView.cs
+++ FitMate.App/ViewModels/WorkoutModelView.cs
@@ -26,7 +26,14 @@ public partial class WorkoutModelView : ObservableObject, IQueryAttributable
             throw new InvalidOperationException("Workout ID needs to be set at least once!");
         }
 
-        LoadWorkout();
+        try
+        {
+            LoadWorkout();
+        }
+        catch (PopupException e)
+        {
+            EmptyWorkoutMessage = e.Message;
+        }
     }
 
     private void LoadWorkout()
@@ -37,9 +44,8 @@ public partial class WorkoutModelView : ObservableObject, IQueryAttributable
 
         EmptyWorkoutMessage = "Loading ...";
 
-        SqlCommand command = new("SELECT e.KgsOrMtr, e.RepsOrSecs, et.ID, et.Name, et.MeasurementTypeID " +
-                                 "FROM Exercises e " +
-                                 "JOIN ExerciseTypes et ON e.ExerciseTypeID = et.ID " +
+        SqlCommand command = new("SELECT e.ID, e.KgsOrMtr, e.RepsOrSecs, et.ID as etID, et.Name, et.MeasurementTypeID " +
+                                 "FROM Exercises e " + "JOIN ExerciseTypes et ON e.ExerciseTypeID = et.ID " +
                                  "WHERE WorkoutID = @wID");
         command.Parameters.AddWithValue("@wID", WorkoutID);
 
@@ -47,11 +53,12 @@ public partial class WorkoutModelView : ObservableObject, IQueryAttributable
         {
             exercises.Add(new Models.Exercise
             {
+                ID = Convert.ToInt32(reader["ID"]),
                 KgsOrMtr = Convert.ToInt32(reader["KgsOrMtr"]),
                 RepsOrSecs = Convert.ToInt32(reader["RepsOrSecs"]),
                 ExerciseType = new Models.ExerciseType
                 {
-                    ID = Convert.ToInt32(reader["ID"]),
+                    ID = Convert.ToInt32(reader["etID"]),
                     Name = Convert.ToString(reader["Name"]) ??
                            throw new SqlNullValueException("The ExerciseType.Name is null!"),
                     MeasurementTypeID = Convert.ToInt32(reader["MeasurementTypeID"])
@@ -65,6 +72,22 @@ public partial class WorkoutModelView : ObservableObject, IQueryAttributable
                          .Select(g => new ExerciseGroup(g.Key.ToString(), g.ToList())))
             {
                 Exercises.Add(group);
+            }
+
+            for (int i = 0; i < Exercises.Count; ++i)
+            {
+                int typeID = Exercises[i][0].ExerciseType.ID;
+                int prID = PersonalRecordFinder.FindForExerciseID(typeID);
+
+                for (int ii = 0; ii < Exercises[i].Count; ++ii)
+                {
+                    if (Exercises[i][ii].ID != prID)
+                    {
+                        continue;
+                    }
+
+                    Exercises[i][ii].IsPR = true;
+                }
             }
         }
         else
